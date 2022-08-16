19d931f0f67db830ca8350689c8a2ef052353c6b
ryu-g <em.ryu.g@gmail.com>
2022-08-16 17:24:12 +0900

    add files to main

---
+++ displayColorCode.js
@@ -0,0 +1,18 @@
+// displayColorCodeOfObject.js
+// const isItemSelected = IS_ITEM_SELECTED() //bool
+// if(isItemSelected){
+//   const selectedPathItemList = ITEMLIST()//pathOBJ
+// }
+// for item in selectedPathItemList{
+//   let x = item.x // posX
+//   let y = item.y // posY
+//   let colorCode = item.filledColor
+//   let textSize = 10
+//   putText(x,y,${colorCode})
+// }
+
+// このファイルのフォルダパス
+var fileDir =  (new File($.fileName)).parent
+var thisApp = BridgeTalk.appName
+alert("こんにちわｗ")
+alert(thisApp)
---
+++ sample.js
@@ -0,0 +1,6 @@
+// このファイルのフォルダパス
+var fileDir =  (new File($.fileName)).parent
+var thisApp = BridgeTalk.appName
+alert("hello from vscode")
+alert(thisApp)
+
